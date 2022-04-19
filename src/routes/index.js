c36aeec0c2d828ca3512fc74f0f7c5cdd47b6ee2
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-04-18 22:03:21 -0500

    Add bcrypt & jwt

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const productsRouter = require("./products");
 const usersRouter = require("./users");
 const categoriesRouter = require("./categories");
+const authRouter = require("./auth");
 
 const apiRouter = (app) => {
   app.use("/products", productsRouter);
   app.use("/users", usersRouter);
   app.use("/categories", categoriesRouter);
+  app.use("/auth", authRouter);
 };
 
 module.exports = apiRouter;
--- src/usecases/user/index.js
+++ src/usecases/user/index.js
@@ -1,11 +1,24 @@
 const User = require("../../models/user").model;
+const encrypt = require("../../lib/encrypt");
 
 const getById = async (id) => {
   return await User.findById(id).exec();
 };
 
+const getByEmail = async (email) => {
+  return await User.findOne({ email }).exec();
+};
+
+const authenticate = async (user, password) => {
+  hash = user.password;
+
+  return await encrypt.verifyPassword(password, hash);
+};
+
 const create = async (firstName, lastName, email, password) => {
-  const user = new User({ firstName, lastName, email, password });
+  const hash = await encrypt.hashPassword(password);
+
+  const user = new User({ firstName, lastName, email, password: hash });
 
   return await user.save();
 };
@@ -13,4 +26,6 @@ const create = async (firstName, lastName, email, password) => {
 module.exports = {
   getById,
   create,
+  getByEmail,
+  authenticate,
 };
